e15f32b798b6f8c97ab7b3aa409a112b97e32dce
weihanran <scintilla_xj@foxmail.com>
2023-08-25 15:08:08 +0800

    feat:添加controller

---
+++ src/main/java/com/easypan/controller/FileInfoController.java
@@ -0,0 +1,17 @@
+package com.easypan.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+/**
+ * 文件信息 Controller
+ */
+@RestController("fileInfoController")
+@RequestMapping("/file")
+public class FileInfoController  {
+
+
+}
